afa69ce101746f713f088feaa7c2f7ffbc0b5a4d
Caleb Paul <calebpaul21@gmail.com>
2016-11-24 12:58:44 -0800

    add api key in gradle properties, add OkHttp to gradle build file

---
+++ app/src/main/java/com/example/guest/myrestaurants/Constants.java
@@ -0,0 +1,11 @@
+package com.example.guest.myrestaurants;
+
+/**
+ * Created by calebpaul on 11/24/16.
+ */
+public class Constants {
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
+}
